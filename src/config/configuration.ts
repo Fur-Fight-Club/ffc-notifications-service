4459c6f4b6e057aa67ded47f84041b2f40df654a
Milan <pro@turtletv.fr>
2023-05-01 18:43:04 +0200

    ✨ Update `.env.example` + config file

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -1,7 +1,7 @@
 export default () => ({
   app_port: 4003 ?? process.env.APP_PORT,
   service: "ffc-notifications-service",
-  issuer: "http://localhost:4000" ?? process.env.ISSUER,
+  issuer: "http://localhost:4003" ?? process.env.ISSUER,
   authorizedServices: ["ffc-analytics-service", "ffc-auth-service", "ffc-main-service", "ffc-notifications-service", "ffc-payments-service"],
   ffc_analytics_url: "http://localhost:4001" ?? process.env.FFC_ANALYTICS_URL,
   ffc_auth_url: "http://localhost:4002" ?? process.env.FFC_AUTH_URL,
