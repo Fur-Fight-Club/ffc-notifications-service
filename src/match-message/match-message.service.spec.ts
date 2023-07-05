2106848841223d7f212f36b22e9808fce1991935
Milan <pro@turtletv.fr>
2023-07-05 21:50:58 +0200

    ✅ Implement match messages tests

--- src/match-message/match-message.service.spec.ts
+++ src/match-message/match-message.service.spec.ts
@@ -6,6 +6,7 @@ import { ConfigService } from "@nestjs/config";
 
 describe("MatchMessageService", () => {
   let service: MatchMessageService;
+  let prismaService: PrismaService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -18,9 +19,53 @@ describe("MatchMessageService", () => {
     }).compile();
 
     service = module.get<MatchMessageService>(MatchMessageService);
+    prismaService = module.get<PrismaService>(PrismaService);
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
   });
 
   it("should be defined", () => {
     expect(service).toBeDefined();
+  });
+
+  describe("create", () => {
+    it("should create a new match message and send it to the client", async () => {
+      const createMatchMessageDto = {
+        message: "Test message",
+        sender: 1,
+        match: 1,
+      };
+
+      const createdMatchMessage = {
+        id: 1,
+        message: "Test message",
+        fk_match: 1,
+        fk_user: 1,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      };
+
+      const prismaCreateMock = jest
+        .spyOn(prismaService.matchMessage, "create")
+        .mockResolvedValue(createdMatchMessage);
+
+      const result = await service.create(createMatchMessageDto);
+
+      expect(prismaCreateMock).toHaveBeenCalledWith({
+        data: {
+          message: "Test message",
+          User: {
+            connect: { id: 1 },
+          },
+          Match: {
+            connect: { id: 1 },
+          },
+        },
+      });
+
+      expect(result).toEqual(createdMatchMessage);
+    });
   });
 });
